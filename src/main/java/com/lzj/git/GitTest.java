fff36349984dc991a8430e93d8d4dc8252334570
lzj <927940872.@qq.com>
2021-09-24 17:05:22 +0800

    second commit

--- src/main/java/com/lzj/git/GitTest.java
+++ src/main/java/com/lzj/git/GitTest.java
@@ -2,6 +2,7 @@ package com.lzj.git;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("halle git");
+		System.out.println("hallo git");
+		System.out.println("hallo git1");
 	}
 }
